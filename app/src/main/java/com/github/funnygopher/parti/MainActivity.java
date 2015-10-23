9a3e2427555062c14653a9d5af42fe1d0675dfa7
Jackkell <bronzeeagle1@hotmail.com>
2015-10-22 21:23:16 -0700

    fixed an old import

--- app/src/main/java/com/github/funnygopher/parti/MainActivity.java
+++ app/src/main/java/com/github/funnygopher/parti/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.github.funnygopher.parti.dummy.DummyFragment;
 import com.github.funnygopher.parti.event.EventListFragment;
 
 public class MainActivity extends AppCompatActivity {
