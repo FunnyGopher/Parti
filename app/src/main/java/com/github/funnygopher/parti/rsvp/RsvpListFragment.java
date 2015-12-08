4395bdc552e2dd5dd9d8cb77d24475739d5707af
FunnyGopher <huntsman.kyle@gmail.com>
2015-12-08 13:17:40 -0700

    Removed the FloatingActionButton from the Rsvp fragment.

--- app/src/main/java/com/github/funnygopher/parti/rsvp/RsvpListFragment.java
+++ app/src/main/java/com/github/funnygopher/parti/rsvp/RsvpListFragment.java
@@ -1,7 +1,6 @@
 package com.github.funnygopher.parti.rsvp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,7 +12,6 @@ import com.github.funnygopher.parti.R;
 import com.github.funnygopher.parti.model.Event;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 
 public class RsvpListFragment extends Fragment {
 
@@ -29,32 +27,9 @@ public class RsvpListFragment extends Fragment {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getBaseContext());
         recyclerView.setLayoutManager(linearLayoutManager);
 
-        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.rsvp_list_test_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fabAction();
-            }
-        });
-
         mRecyclerAdapter = new RsvpRecyclerAdapter(getContext(), new ArrayList<Event>());
         recyclerView.setAdapter(mRecyclerAdapter);
 
         return view;
-    }
-
-    private void fabAction() {
-        // Creating a dummy event for the crazy rave
-        Calendar raveStartDate = Calendar.getInstance();
-        raveStartDate.set(2015, 9, 31, 22, 0, 0);
-        Calendar raveEndDate = Calendar.getInstance();
-        raveEndDate.set(2015, 10, 6, 22, 0, 0);
-        Event event = new Event(
-                "The Crazy Rave", "The Rave Boys",
-                "The craziest rave you've ever been to. Strap on your leaderhosen. It's about to get bumpy.",
-                "$5.00 at the door. Glowsticks are a must.",
-                raveStartDate, raveEndDate,
-                33.3775468, -111.9811847, 0, 0);
-        mRecyclerAdapter.add(event);
     }
 }
